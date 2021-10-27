b7c2f03d8b496d5e8a87de61c05b3b4a8c2bbdee
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-10-27 13:20:18 +0600

    Update and rename image.php to rustam.php

---
+++ rustam.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+yvgtuh87yu
+
+
+<h2>Portfolio</h2>
+<img src="x-special/nautilus-clipboard
+copy
+file:///home/student/index.jpeg
+" alt="Flowers in Chania" width="460" height="345">
+
+</body>
+</html>
