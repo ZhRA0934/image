35343115a4be8963e6bae0c4d9791b9dc8f87244
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-10-27 13:54:10 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <body style="background-color:powderblue;">
 
 
-<h2 style="color:blue;">Portfolio</h2>
+<h2 style="color:black;">Portfolio</h2>
 <p style="color:red;">Informatin</p>
 
 <a href="https://www.w3schools.com">Visit W3Schools</a>
 
-<img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">
+<img src="index.jpeg" alt="W3Schools.com" width="104" height="142">
 
 <button onclick="document.location='default.asp'">HTML Tutorial</button>
 
